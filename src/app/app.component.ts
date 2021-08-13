6312a70d04d28743cf355ecd834ba15d21b3b88b
IanKlek <ian.klek@gmail.com>
2021-08-13 21:22:43 +0100

    Random number woohoo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Apollo, gql} from 'apollo-angular';
+import {Observable} from 'rxjs/internal/Observable';
+import { ApolloQueryResult } from '@apollo/client/core';
+import {tap} from 'rxjs/operators';
 
 @Component({
   selector: 'gql-poc-root',
@@ -7,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gql-poc-ng';
+
+  random$: Observable<ApolloQueryResult<{ random: number }>>;
+
+  constructor(private apollo: Apollo) {
+    this.random$ = apollo.watchQuery<{ random: number }>({
+      query: gql`
+          {
+            random
+          }
+        `,
+    }).valueChanges.pipe(
+      tap(e => console.log(e)),
+    );
+  }
 }
